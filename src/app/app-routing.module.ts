e611bbd2e8b6a785e6bbdd4a2943933549e9640b
TomasP <perezgantomas@gmail.com>
2021-11-03 08:14:49 -0400

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './layouts/homepage/homepage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home-page', component: HomepageComponent },
+  {path: '', redirectTo: '/home-page', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
